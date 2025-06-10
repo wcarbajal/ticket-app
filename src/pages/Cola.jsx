c373f9873e12b131f6aca0dbbada19a30e901246
wcarbajal <wcarbajal@outlook.com>
2025-06-10 17:12:58 -0500

    en ColaPage

--- src/pages/Cola.jsx
+++ src/pages/Cola.jsx
@@ -1,5 +1,85 @@
+import { Card, Col, List, Row, Typography, Tag } from "antd";
+
+const { Title, Text } = Typography;
+
+
+const data = [
+  {
+    ticketNo: 33,
+    escritorio: 3,
+    agente: 'Fernando Herrera'
+  },
+  {
+    ticketNo: 34,
+    escritorio: 4,
+    agente: 'Melissa Flores'
+  },
+  {
+    ticketNo: 35,
+    escritorio: 5,
+    agente: 'Carlos Castro'
+  },
+  {
+    ticketNo: 36,
+    escritorio: 3,
+    agente: 'Fernando Herrera'
+  },
+  {
+    ticketNo: 37,
+    escritorio: 3,
+    agente: 'Fernando Herrera'
+  },
+  {
+    ticketNo: 38,
+    escritorio: 2,
+    agente: 'Melissa Flores'
+  },
+  {
+    ticketNo: 39,
+    escritorio: 5,
+    agente: 'Carlos Castro'
+  },
+];
+
 export const ColaPage = () => {
+
   return (
-    <div>ColaPage</div>
-  )
-}
+    <>
+      <Title level={ 1 } > Atendiento al cliente
+
+      </Title>
+
+      <Row>
+        <Col span={ 12 }>
+          <List
+            dataSource={ data.slice( 0, 3 ) }
+            renderItem={ item => (
+              <List.Item>
+                <Card
+                  style={ { width: 400, marginTop: 16 } }
+                  actions={[
+                  <Tag color="volcano" key="numero">{item.ticketNo}</Tag>,
+                  <Tag color="magenta" key="escritorio">{item.escritorio}</Tag>,
+                  <Tag color="geekblue" key="agente">{item.agente}</Tag>
+                ]}
+                >
+
+                  
+                </Card>
+              </List.Item>
+            ) }
+
+          />
+
+        </Col>
+        <Col span={ 12 }>
+
+        </Col>
+      </Row>
+
+      <Text >
+
+      </Text>
+    </>
+  );
+};
--- src/pages/CrearTicket.jsx
+++ src/pages/CrearTicket.jsx
@@ -1,18 +1,47 @@
-import { Row, Col, Typography } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
+import { Row, Col, Typography, Button } from 'antd';
 
 const { Title, Text } = Typography;
 
 export const CrearTicket = () => {
+  
+  const nuevoTicket = () => {
+    console.log("jala ticket")
+  }
+  
   return (
     <>
 
       <Row>
-        <Col span={ 14 } offset= { 6 } align= "center">
-        <Title >
-          Presiones el botton para un nuevo ticket
-        </Title>
+        <Col span={ 14 } offset={ 6 } align="center">
+          <Title level={ 3 } >
+            Presiones el botton para un nuevo ticket
+          </Title>
+          <Button
+            type="primary"
+            shape="round"
+            icon={ <DownloadOutlined />}
+            size= "large"
+            onClick={ nuevoTicket}
+
+          >
+            Jalar ticket
+          </Button>
 
 
+        </Col>
+      </Row>
+      <Row style={{ marginTop: 100 }}>
+        <Col span={14} offset= {6} align="center">
+
+        <Text   level={2}>
+          Su numero es: 
+        </Text>
+        <br />
+        <Text   type="success" style={{ fontSize: 55}}>
+          55
+        </Text>
+        
         </Col>
       </Row>
 
