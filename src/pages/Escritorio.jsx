9f9a488e24e71fdca8eb098fb3587120fc398e02
wuilmer carbajal <wcarbajal@outlook.com>
2025-06-09 22:45:35 -0500

    aun en el escritorio antes del downgrade

--- src/pages/Escritorio.jsx
+++ src/pages/Escritorio.jsx
@@ -1,22 +1,71 @@
-import { Row, Col, Typography } from 'antd';
+import { CloseCircleOutlined, RightOutlined } from '@ant-design/icons';
+import { Row, Col, Typography, Button, Divider } from 'antd';
 
 const { Title, Text } = Typography;
 
 export const EscritorioPage = () => {
+
+  const salir = () => {
+    console.log( "salir" );
+  };
+
+  const siguienteTicket = () => {
+    console.log( "siguiente" );
+  };
   return (
     <>
       <Row>
         <Col span={ 20 }>
           <Title level={ 2 }> Wuilmer</Title>
           <Text> Usted está trabajando en el escritorio</Text>
-          <Text>: 5</Text>
+          <Text type="success">: 5</Text>
         </Col>
 
-        <Col >
-        
+        <Col span={ 4 } align="right" >
+          <Button
+            shape="round"
+            variant="solid"
+            color="danger"
+            onClick={ salir }
+          >
+            <CloseCircleOutlined />
+            Salir
+
+          </Button>
+
         </Col>
 
       </Row>
+
+      <Divider />
+
+      <Row>
+        <Col>
+          <Text > Está atendiendo el ticket número: </Text>
+          <Text
+            style={ { fontSize: 30 } }
+            type="danger"
+          >
+            65
+          </Text>
+        </Col>
+      </Row>
+
+      <Row>
+
+        <Col offset={ 18 } span={ 6 } align="right">
+          <Button
+            onClick={ siguienteTicket }
+            shape= "round"
+            type="primary"
+          >
+            <RightOutlined />
+            Siguiente
+          </Button>
+
+        </Col>
+      </Row>
+
     </>
   );
 };
